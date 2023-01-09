62e5f0cef398504d465505e6041bef885d54726e
TusahrCitrusbug <tushar.citrusbug@gmail.com>
2023-01-09 15:22:29 +0530

    updated package

--- index.js
+++ index.js
@@ -1,3 +1,25 @@
 exports.helloFunction = (your_name) => {
     console.log(`Hello ${your_name}. How are you.`);
 }
+const fs = require('fs');
+const path = require('path');
+const directoryPath = path.join(__dirname, 'Documents');
+console.log(directoryPath);
+console.log(__dirname);
+// const inquirer = require('inquirer');
+// const globle_input = async () => {
+//     const email_input = await inquirer
+//         .prompt([
+//             {
+//                 type: 'text',
+//                 name: 'path',
+//                 message: 'Enter the path of your project/files:',
+//             },
+//         ])
+//     console.log(email_input);
+// }
+// async function CreateSuperUser() {
+//     await globle_input();
+// };
+
+// CreateSuperUser();
